3f52562a1736d3a11facb117ffccfefb3361c93a
Ruslan <ruslan.leonoff2014@yandex.ru>
2023-12-04 17:41:29 +0300

    lesson 9

---
+++ lesson9/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace project {
+    
+    class Program {
+
+        static void Main(){
+            short [,] nums ={
+                {5, 6, 7},
+                {8, 5, 4},
+                {2, 8, 3},
+            }   
+
+            foreach(short el in nums) {
+                System.Console.WriteLine ("El: "+ el);
+            }
+        }
+    }
+}
