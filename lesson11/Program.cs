e501951af9204f033b8d1d04db9e5511cad3bc09
Ruslan <ruslan.leonoff2014@yandex.ru>
2023-12-09 21:16:18 +0300

    new project and a completed old one

---
+++ lesson11/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace project {
+
+    class Program {
+
+        static void Main() {
+            string word = "Hello";
+            word += "!!";
+
+            // Console.WriteLine(word[1]);
+            // word = (string.Concat(word, "!"));
+            // Console.WriteLine(string.Compare(word, "Hello"));
+
+            string peop
+        }
+    }
+}
