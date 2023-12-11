b0a7ad3b806cb84a08129b9d0ffb03a7d8c9f16d
Ruslan <ruslan.leonoff2014@yandex.ru>
2023-12-11 17:49:48 +0300

    word for strind

---
+++ lesson11/project2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace project
+{
+
+    class Program
+    {
+
+        static void Main()
+        {
+            Console.WriteLine("Enter text: ");
+            string text = Console.ReadLine();
+            using (FileStream stream = new FileStream("into.txt", FileMode.OpenOrCreate))
+            {
+                byte[] array = System.Text.Encoding.Default.GetBytes(text);
+
+                stream.Write(array, 0, array.Length);
+            }
+        }
+    }
+}
